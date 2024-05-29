3df55c117eecfd8f4c063172c695c676543936a9
sujungkim99 <161662701+SujungKim99@users.noreply.github.com>
2024-05-29 11:49:29 +0900

    0529

--- gameshop/src/main/resources/static/js/login.js
+++ gameshop/src/main/resources/static/js/login.js
@@ -19,8 +19,31 @@ document.querySelector(".loginBtn").addEventListener("click", () => {
     .post(urlLogin, data, { withCredentials: true })
     .then((response) => {
       console.log("데이터: ", response);
+      sessionCurrent();
     })
     .catch((error) => {
       console.log("에러 발생: ", error);
     });
 });
+function sessionCurrent() {
+  axios
+    .get("http://localhost:8080/user/current", { withCredentials: true })
+    .then((response) => {
+      console.log("데이터 : ", response);
+      if (response.status == 200) {
+        console.log("세션유지");
+        if (response.status == 200) {
+          document.querySelector(".login-box").classList.add("hidden");
+          document.querySelector(".user-box").classList.remove("hidden");
+          document.querySelector(".user-box p").textContent =
+            response.data + "님, 환영합니다.";
+        }
+      }
+    })
+    .catch((error) => {
+      console.log("에러발생 : ", error);
+    });
+}
+
+//js 파일이 로드 될때 호출됨
+sessionCurrent();
